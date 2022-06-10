aeb8100ad85de64f6ca5b9a7573fc7150799e9e3
Divya <div.shivaram@gmail.com>
2022-06-10 16:14:56 +0530

    fix accept username and load game

--- api/urls.py
+++ api/urls.py
@@ -1,13 +1,15 @@
 from django.urls import path
 from rest_framework.urlpatterns import format_suffix_patterns
-from .views import create_or_join_game, get_all_games, index
+from .views import create_or_join_game, render_game, get_all_games, index
 
 # Create your views here.
 
 urlpatterns = {
     path('', index, name="index"),
     path('allgames', get_all_games, name="items"),
+    path('games/<slug:game_id>', render_game, name="get_game"),
     path('createUser', create_or_join_game, name="createUser")
+    # path('users', create_or_join_game, name="createUser"),
 }
 
 urlpatterns = format_suffix_patterns(urlpatterns)
--- api/views.py
+++ api/views.py
@@ -1,4 +1,5 @@
 # from http.client import HTTPResponse
+from .forms import NameForm
 import json
 from venv import create
 from django.conf import settings
@@ -6,8 +7,8 @@ import redis
 from rest_framework.decorators import api_view
 from rest_framework import status
 from rest_framework.response import Response
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.shortcuts import render, redirect
 
 # connect to our Redis instance
 redis_instance = redis.StrictRedis(host=settings.REDIS_HOST,
@@ -16,28 +17,45 @@ redis_instance = redis.StrictRedis(host=settings.REDIS_HOST,
 
 def index(request, *args, **kwargs):
     context = {'moves': []}
-    return render(request, 'board.html', context)
+    form = NameForm(request.POST)
+    print("FORM: ", request)
+    return render(request, 'landing.html', context)
 
 
 @api_view(['POST'])
 def create_or_join_game(request, *args, **kwargs):
-    """
-    accepts username in request body and returns a gameId with response
-    """
-
-    player_name = json.loads(request.body)['username']
+    form = NameForm(request.POST)
+    username = form.data['your_name']
+    player_name = username
     last_game = get_last_game()
 
-    if last_game is None or last_game['O'] is None:
+    if last_game is None or last_game['O'] is not None:
         game_id = create_new_game(player_name)
         # update_game_id_in_storage()
     else:
         game_id = redis_instance.get('GameId')
 
-    print("ITEM: ", player_name, 'Game ID: ', game_id)
-
+    game_id = str(int(game_id))
+    # add user to 'O'
     # TODO: redirect to game page game_id
-    return Response(status=200)
+    return redirect('get_game', game_id=game_id)
+
+
+@api_view(['GET'])
+def render_game(request, game_id):
+    context = {'moves': get_game_object(game_id)['moves']}
+    # TODO: Handle error here. Page does not exist, throw exception etc
+    # example:
+    # from django.http import Http404
+    # try:
+    #     board = board.objects.get(pk=game_id)
+    # except board.DoesNotExist:
+    #     raise Http404("Game does not exist")
+    return render(request, 'board.html', context)
+
+
+def get_game_object(game_id):
+    return json.loads(redis_instance.get('Games'))[str(int(game_id))]
 
 
 @api_view(['GET'])
@@ -62,12 +80,17 @@ def update_moves(request, *args, **kwargs):
     return
 
 
+def add_second_player(player_name, game_id):
+
+    return
+
+
 def get_last_game():
     # Could be a part of a class
     last_game_id = 0
     if redis_instance.exists('GameId'):
         last_game_id = redis_instance.get('GameId')
-        return json.loads(redis_instance.get('Games'))[str(int(last_game_id))]
+        return get_game_object(last_game_id)
 
     return None
 
@@ -75,9 +98,11 @@ def get_last_game():
 def create_new_game(player_name):
     # player_name is the first player
     last_game_id = 0
+    games = {}
     if redis_instance.exists('GameId'):
         last_game_id = redis_instance.get('GameId')
-    games = json.loads(redis_instance.get('Games'))
+    if redis_instance.exists('Games'):
+        games = json.loads(redis_instance.get('Games'))
     new_game_id = str(int(last_game_id)+1)
     new_game = {"X": player_name, "O": None,
                 "moves": ["" for x in range(8)]}
@@ -85,3 +110,5 @@ def create_new_game(player_name):
 
     redis_instance.set('Games', json.dumps(games))
     redis_instance.set('GameId', new_game_id)
+
+    return new_game_id
